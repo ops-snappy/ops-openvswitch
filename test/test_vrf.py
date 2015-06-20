f79adf954e22952834886a6878e07804b6aadf8c
Chandra Shekhar <chandra.shekhar@hp.com>
2015-06-19 23:57:16 -0700

    Test to add/remove primary ipv4/ipv6 address and secondary ipv4 address.
    Change-Id: I8ab765be134053810d60a636fe5cf7526f8fe8dc
    Signed-off-by: Chandra Shekhar <chandra.shekhar@hp.com>

--- test/test_vrf.py
+++ test/test_vrf.py
@@ -1,3 +1,20 @@
+"""
+Copyright (C) 2015 Hewlett Packard Enterprise Development LP
+All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+"""
+
 #!/usr/bin/python
 
 import os
@@ -30,7 +47,7 @@ class vrfTests( HalonTest ):
     #configuring Halon, in the future it would be through
     #proper Halon commands
     s1 = self.net.switches[ 0 ]
-    time.sleep(5)
+    #time.sleep(5)
 
     #Configure vrf
     print('Configure a vrf vrf1 in DB')
@@ -121,7 +138,7 @@ class vrfTests( HalonTest ):
     output = s1.cmd("/usr/bin/ovs-vsctl add-port br1 1");
 
     if 'constraint violation' in output:
-      print('Success: Can\'t add port to bridge br1 as it belongs to vrf vrf1')
+      print('Success: Can\'t add port to bridge br1, belongs to vrf vrf1')
     else:
       print(output)
       assert 0, 'Failed: Port 1 added to br1 and already in vrf vrf1'
@@ -146,9 +163,85 @@ class vrfTests( HalonTest ):
     print('*** End of vrf-port commands ***')
     print('=============================================')
 
-  # Extra cleanup if test fails in middle
+  def test_vrf_intf_ip_address(self):
+    s1 = self.net.switches[ 0 ]
+
+    print('\n=============================================')
+    print('*** Test ip/ipv6 address on port ***')
+    print('=============================================')
+    s1.cmd("/usr/bin/ovs-vsctl add-vrf vrf1")
+    s1.cmd("/usr/bin/ovs-vsctl add-vrf-port vrf1 1")
+
+    #Configure and check primary ip address
+    print('Configure primary ip address 10.1.1.1/24 on port 1')
+    s1.cmd("/usr/bin/ovs-vsctl set port 1 ip_address=10.1.1.1/24")
+    output = s1.cmd("ip netns exec swns ip addr show dev 1 primary")
+    if '10.1.1.1/24' in output:
+      print('Primary address 10.1.1.1/24 configured on port 1')
+    else:
+      assert 0, 'Primary ip address configuration failed'
+
+    #Configure and check secondary ip address
+    print('Configure secondary ip address 10.1.1.2/24 on port 1')
+    s1.cmd("/usr/bin/ovs-vsctl set port 1 ip_address_second=10.1.1.2/24")
+    output = s1.cmd("ip netns exec swns ip addr show dev 1 secondary")
+    if '10.1.1.2/24' in output:
+      print('Secondary address 10.1.1.2/24 configured on port 1')
+    else:
+      assert 0, 'Secondary ip address configuration failed'
+
+    #Configure and check ipv6 address
+    print('Configure IPv6 address 2001::1/64 on port 1')
+    s1.cmd("/usr/bin/ovs-vsctl set port 1 ip6_address='2001\:\:1/64'")
+    output = s1.cmd("ip netns exec swns ip -6 addr show dev 1")
+    if '2001::1/64' in output:
+      print('IPv6 address 2001::1/64 configured on port 1')
+    else:
+      assert 0, 'IPv6 address configuration failed'
+
+    #Delete ipv6 address
+    print('Unconfigure IPv6 address 2001::1/64 on port 1')
+    s1.cmd("/usr/bin/ovs-vsctl set port 1 ip6_address=[]")
+    output = s1.cmd("ip netns exec swns ip -6 addr show dev 1")
+    if '2001::1/64' not in output:
+      print('IPv6 address 2001::1/64 unconfigured on port 1')
+    else:
+      assert 0, 'IPv6 address unconfiguration failed'
+
+    #Delete ipv4 secondary address
+    print('Unconfigure secondary ip address 10.1.1.2/24 on port 1')
+    s1.cmd("/usr/bin/ovs-vsctl set port 1 ip_address_second=[]")
+    output = s1.cmd("ip netns exec swns ip addr show dev 1 secondary")
+    if '10.1.1.2/24' not in output:
+      print('Secondary address 10.1.1.2/24 unconfigured on port 1')
+    else:
+      assert 0, 'Secondary ip address unconfiguration failed'
+
+    #Delete ipv4 primary address
+    print('Unconfigure primary ip address 10.1.1.1/24 on port 1')
+    s1.cmd("/usr/bin/ovs-vsctl set port 1 ip_address=[]")
+    output = s1.cmd("ip netns exec swns ip addr show dev 1 primary")
+    if '10.1.1.1/24' not in output:
+      print('Primary address 10.1.1.1/24 unconfigured on port 1')
+    else:
+      assert 0, 'Primary ip address unconfiguration failed'
+
+    #Delete port from vrf and also remove vrf
+    s1.cmd("/usr/bin/ovs-vsctl del-vrf-port vrf1 1")
+    s1.cmd("/usr/bin/ovs-vsctl del-vrf vrf1")
+
+    print('=============================================')
+    print('*** End of ip/ipv6 commands ***')
+    print('=============================================')
+
+  #Extra cleanup if test fails in middle
   def vrf_cleanup(self):
     s1 = self.net.switches[ 0 ]
+
+    output = s1.cmd("ip netns exec swns ip addr show dev 1")
+    if 'inet' in output:
+      s1.cmd("ip netns exec swns ip address flush dev 1")
+
     output = s1.cmd("/usr/bin/ovs-vsctl list-vrf")
     if 'vrf1' in output:
       s1.cmd("/usr/bin/ovs-vsctl del-vrf vrf1")
@@ -156,7 +249,6 @@ class vrfTests( HalonTest ):
     output = s1.cmd("/usr/bin/ovs-vsctl list-br")
     if 'br1' in output:
       s1.cmd("/usr/bin/ovs-vsctl del-br1 br1")
-
 
 
 class Test_vrf:
@@ -193,3 +285,5 @@ class Test_vrf:
   def test_vrf_port_config_commands(self):
     self.test.vrf_port_config_commands()
 
+  def test_vrf_intf_ip_address(self):
+    self.test.test_vrf_intf_ip_address()
