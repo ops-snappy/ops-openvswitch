95555e8a3e1eab05b3f3feb5d7c15dcd25495ff0
Vasanth Viswanathan <vasanth.viswanathan@hpe.com>
2015-09-28 19:58:47 -0700

    PEP8 compliance and License Changes
    This commit includes PEP8 compliance on all scripts.
    Modified License Information.
    Removed Halon references
    
    Change-Id: Id12aeda56cc8d097fe8f0c433ffb22682a3bb59d
    Signed-off-by: Vasanth Viswanathan <vasanth.viswanathan@hpe.com>

--- ops/tests/test_openvswitch_ct_socket_permission.py
+++ ops/tests/test_openvswitch_ct_socket_permission.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-# Copyright (C) 2015 Hewlett Packard Enterprise Development LP
+# (c) Copyright 2015 Hewlett Packard Enterprise Development LP
 # All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -19,50 +19,53 @@ from opsvsi.docker import *
 from opsvsi.opsvsitest import *
 from opsvsiutils.systemutil import *
 
-class socketFilePermissionTests( OpsVsiTest ):
 
-  def setupNet(self):
-    # if you override this function, make sure to
-    # either pass getNodeOpts() into hopts/sopts of the topology that
-    # you build or into addHost/addSwitch calls
-    host_opts = self.getHostOpts()
-    switch_opts = self.getSwitchOpts()
-    openswitch_topo = SingleSwitchTopo(k=0, hopts=host_opts, sopts=switch_opts)
-    self.net = Mininet(openswitch_topo, switch=VsiOpenSwitch,
-                       host=Host, link=OpsVsiLink,
-                       controller=None, build=True)
+class socketFilePermissionTests(OpsVsiTest):
 
-  def socket_file_permission_verify(self):
-    info("########## Verify DB socket file permissions ##########\n")
-    # configuring Halon, in the future it would be through
-    # proper Halon commands
-    s1 = self.net.switches[ 0 ]
+    def setupNet(self):
+        # if you override this function, make sure to
+        # either pass getNodeOpts() into hopts/sopts of the topology that
+        # you build or into addHost/addSwitch calls
+        host_opts = self.getHostOpts()
+        switch_opts = self.getSwitchOpts()
+        openswitch_topo = SingleSwitchTopo(
+            k=0, hopts=host_opts, sopts=switch_opts)
+        self.net = Mininet(openswitch_topo, switch=VsiOpenSwitch,
+                           host=Host, link=OpsVsiLink,
+                           controller=None, build=True)
 
-    # Check permissions of socket files present in /var/run/openvswitch
-    output = s1.cmd("ls -l /var/run/openvswitch")
-    lines = output.split('\n')
-    for line in lines:
-        if 'srwxrw' in line:
-           if 'ovsdb_users' not in line:
-              info("Test failed!")
-              return
+    def socket_file_permission_verify(self):
+        info("########## Verify DB socket file permissions ##########\n")
+        # configuring OpenSwitch, in the future it would be through
+        # proper OpenSwitch commands
+        s1 = self.net.switches[0]
 
-    info("########## File permissions and group are valid for socket files ##########\n")
+        # Check permissions of socket files present in /var/run/openvswitch
+        output = s1.cmd("ls -l /var/run/openvswitch")
+        lines = output.split('\n')
+        for line in lines:
+            if 'srwxrw' in line:
+                if 'ovsdb_users' not in line:
+                    info("Test failed!")
+                    return
+
+        info("########## File permissions and group are valid "
+             "for socket files ##########\n")
 
 
 class Test_socket_file_permissions:
 
-  def setup_class(cls):
-    Test_socket_file_permissions.test = socketFilePermissionTests()
+    def setup_class(cls):
+        Test_socket_file_permissions.test = socketFilePermissionTests()
 
-  # Test for socket file permissions
-  def test_socket_file_permission_verify(self):
-    self.test.socket_file_permission_verify()
+    # Test for socket file permissions
+    def test_socket_file_permission_verify(self):
+        self.test.socket_file_permission_verify()
 
-  def teardown_class(cls):
-    # Stop the Docker containers, and
-    # mininet topology
-    Test_socket_file_permissions.test.net.stop()
+    def teardown_class(cls):
+        # Stop the Docker containers, and
+        # mininet topology
+        Test_socket_file_permissions.test.net.stop()
 
-  def __del__(self):
-    del self.test
+    def __del__(self):
+        del self.test
