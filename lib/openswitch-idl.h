OpenSwitchNOS/openswitch-ops-openvswitch

add define required by US 3410 Configure LAG with fallback
Change-Id: I17da1b3f4e5b88ae21b26a555fb79afbaf8899b6
Signed-off-by: Erick Castro <erick.castro@hpe.com>

lib/openswitch-idl.h
@@ -135,6 +135,8 @@ enum ovsrec_interface_pm_info_power_mode_e {
 #define INTERFACE_OTHER_CONFIG_MAP_LACP_PORT_ID                 "lacp-port-id"
 #define INTERFACE_OTHER_CONFIG_MAP_LACP_PORT_PRIORITY           "lacp-port-priority"
 
+#define PORT_OTHER_CONFIG_FALLBACK                              "lacp-fallback"
+
 #define INTERFACE_USER_CONFIG_MAP_ADMIN                         "admin"
 
 #define OVSREC_INTERFACE_USER_CONFIG_ADMIN_DOWN                 "down"

ops-snappy/ops-openvswitch

lib/openswitch-idl.h
@@ -385,6 +385,8 @@ enum ovsrec_interface_hw_bond_config_enabled_e {
 
 #define PORT_OTHER_CONFIG_MAP_LACP_TIME                 "lacp-time"
 
+#define PORT_OTHER_CONFIG_LACP_FALLBACK                 "lacp-fallback"
+
 #define PORT_OTHER_CONFIG_LACP_TIME_SLOW                "slow"
 #define PORT_OTHER_CONFIG_LACP_TIME_FAST                "fast"
 