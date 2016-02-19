OpenSwitchNOS/openswitch-ops-openvswitch

Add reconfigure blocks for bridge ordered execution
This commit is part of SwitchD Refactor.
The change introduces blocks in bridge.c file. Plugin functions
can be registered in the blocks for sequential execution
inside bridge_reconfigure routine. The blocks support priority
levels for ordered execution of callbacks.

Change-Id: If1e2a3be69750beb8c7ba7a56858f85aacec0b6a
Signed-off-by: DiegoSolano <ldsolano17@hpe.com>

lib/automake.mk
@@ -84,6 +84,8 @@ ovslibinclude_HEADERS = \
 	lib/plugins.h \
 	lib/vlan-bitmap.h \
 	libltdl/ltdl.h \
+	lib/reconfigure-blocks.h \
+	lib/vswitch-config.h \
 	vswitchd/bufmon-provider.h
 endif
 
@@ -241,7 +243,10 @@ lib_libovscommon_la_SOURCES = \
         lib/vlog.c \
         lib/vlog.h \
         lib/ipc_packets.c \
-        lib/ipc_packets.h
+        lib/ipc_packets.h \
+        lib/vswitch-config.h \
+        lib/reconfigure-blocks.c \
+        lib/reconfigure-blocks.h
 
 # libovsdb
 lib_LTLIBRARIES += ovsdb/libovsdb.la

ops-snappy/ops-openvswitch

plugins/automake.mk
@@ -8,7 +8,8 @@ if OPS
 ovspluginslibincludedir = $(includedir)/ovs
 ovspluginslibinclude_HEADERS = \
 	plugins/plugins.h \
-	plugins/plugin-extensions.h
+	plugins/plugin-extensions.h \
+	plugins/reconfigure-blocks.h
 endif
 
 lib_LTLIBRARIES += plugins/libplugins.la
@@ -25,7 +26,9 @@ plugins_libplugins_la_SOURCES = \
 	plugins/plugins_yaml.c \
 	plugins/plugins_yaml.h \
 	plugins/plugin-extensions.c \
-	plugins/plugin-extensions.h
+	plugins/plugin-extensions.h \
+	plugins/reconfigure-blocks.c \
+	plugins/reconfigure-blocks.h
 
 plugins_libplugins_la_CFLAGS = -DYAML_PATH=$(sysconfdir)/openswitch/platform
 