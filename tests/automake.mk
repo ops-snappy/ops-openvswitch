b6fabde6e864be199fc7da6863cb29a2cdb25014
Esteban Rodriguez Betancourt <estebarb@hpe.com>
2016-02-26 18:00:52 -0600

    Add tests for skiplist
    Change-Id: I34e629092db5569966420d9215c27be30f341372
    Signed-off-by: Esteban Rodriguez Betancourt <estebarb@hpe.com>

--- tests/automake.mk
+++ tests/automake.mk
@@ -167,6 +167,7 @@ valgrind_wrappers = \
 	tests/valgrind/test-reconnect \
 	tests/valgrind/test-rstp \
 	tests/valgrind/test-sha1 \
+	tests/valgrind/test-skiplist \
 	tests/valgrind/test-stp \
 	tests/valgrind/test-type-props \
 	tests/valgrind/test-unix-socket \
@@ -312,6 +313,7 @@ tests_ovstest_SOURCES = \
 	tests/test-rstp.c \
 	tests/test-sflow.c \
 	tests/test-sha1.c \
+	tests/test-skiplist.c \
 	tests/test-stp.c \
 	tests/test-util.c \
 	tests/test-uuid.c \
